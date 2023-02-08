259b1b37d61acfd68a1b398082126a63c821abc7
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-02-08 13:54:21 +0800

    readd minify

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-./node_modules/.bin/esbuild src/app.js --bundle --outfile=src/build/service.cjs --platform=node --analyze
-./node_modules/.bin/esbuild src/launcher.js --bundle --outfile=build/grader.cjs --platform=node --analyze
+./node_modules/.bin/esbuild src/app.js --bundle --outfile=src/build/service.cjs --platform=node --minify --analyze
+./node_modules/.bin/esbuild src/launcher.js --bundle --outfile=build/grader.cjs --platform=node --minify --analyze
